b664caecd000dcd95edd2ac500f0697b4d248f6c
AAndersonSantos <andersonsantos.inc@gmail.com>
2022-05-16 19:03:15 -0300

    edit project for heroku

--- src/main/java/application/model/Modelo.java
+++ src/main/java/application/model/Modelo.java
@@ -1,19 +1,12 @@
 package application.model;
 
 import java.io.Serializable;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import lombok.Data;
 
@@ -27,13 +20,13 @@ public class Modelo implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long modeloId;
 	
-	@ManyToOne
+	/*@ManyToOne
 	@JoinColumn(name = "marca_id")
-	public Marca marca;
+	public Marca marca;*/
 	
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "modelo")
+	/*@OneToMany(cascade = CascadeType.ALL, mappedBy = "modelo")
 	@JsonBackReference
-    public List<Carro> carro;
+    public List<Carro> carro;*/
 	
 	//@Size(min = 2, message = "{campo.min.modelo}")
 	//@Size(max = 20, message = "{campo.max.modelo}")
